721a5de827311c582ab33aea69bb0b9f0e97d43f
Eddy-Malatjie <40563637+Eddy-Malatjie@users.noreply.github.com>
2018-10-17 16:09:47 +0200

    Update ReadFileTests.cs

--- Assignment.Tests/ReadFileTests.cs
+++ Assignment.Tests/ReadFileTests.cs
@@ -28,7 +28,7 @@ namespace Assignment.Tests
             string expected = "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(400,'Manmay','Mohanty','M',' 07/03/1983',True)"+Environment.NewLine+"insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(401,'Marike','Fourie','F',' 30/11/2001',False)"+ Environment.NewLine;
 
             //act
-            string actual = ReadFile.ReadTypeOne(@"C:\Users\Reverside\Desktop\customers_1.txt", "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status)", 400);
+            string actual = ReadFile.ReadTypeOne(@"C:\customers_1.txt", "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status)", 400);
 
             //Assert
             Assert.Equal(expected, actual);
@@ -41,7 +41,7 @@ namespace Assignment.Tests
             string expected = "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(400,'Manmay','Mohanty','M','07/03/1983',True)" + Environment.NewLine + "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(401,'Marike','Fourie','F','30/11/2001',False)" + Environment.NewLine;
 
             //act
-            string actual = ReadFile.ReadTypeTwo(@"C:\Users\Reverside\Desktop\customers_2.txt", "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status)", 400);
+            string actual = ReadFile.ReadTypeTwo(@"C:\customers_2.txt", "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status)", 400);
 
             //Assert
             Assert.Equal(expected, actual);
@@ -54,7 +54,7 @@ namespace Assignment.Tests
             string expected = "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(400,'Manmay','Mohanty','M','07/03/1983',True)" + Environment.NewLine + "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status) values(401,'Marike','Fourie','F','30/11/2001',False)" + Environment.NewLine;
 
             //act
-            string actual = ReadFile.ReadTypeThree(@"C:\Users\Reverside\Desktop\customers_3.txt", "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status)", 400);
+            string actual = ReadFile.ReadTypeThree(@"C:\customers_3.txt", "insert into customers(id, first_name, last_name, gender, date_of_birth, marital_status)", 400);
 
             //Assert
             Assert.Equal(expected, actual);
